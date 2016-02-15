4a66dacd3170a05b79feb7166606f978ca8873ea
Elisa Navarro Zuara <navarrozuara@gmail.com>
2016-02-15 12:30:15 +0100

    Update ejercicio8.7.js

--- jQuery78/ejercicios/js/ejercicio8.7.js
+++ jQuery78/ejercicios/js/ejercicio8.7.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		capa.delay(2000);
 		capa.slideDown(1000);
 		capa.fadeTo(1500, 0.3).delay(3000).fadeTo(500, 1);
-			capa.delay(500);
+		capa.delay(500);
 		capa.animate({
 			"font-size": "+=0.5em"
 		}, 1000, colaEfectos);
