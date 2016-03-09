618c83182095adab0f735f29e84c7ea190696741
navarrozuara <navarrozuara@gmail.com>
2016-03-09 20:20:09 +0100

    Añadimos ejercicio de Ajax con jQuery

---
+++ AjaxjQuery/js/script.js
@@ -0,0 +1,31 @@
+$(function() {
+
+	var recurso = $('recurso');
+	var contenidos = $('contenidos');
+	var	estados = $('estados');
+	var	limpiar = $('limpiar');
+	var	mostrar = $('enviar');
+
+	recurso.val($(location).attr('href'));
+
+	mostrar.on('click', function() {
+		$.get(recurso.val())
+			.done(function(data) {
+				contenidos.text(data);
+				estados.text(estados.html() + 'Done\n');
+			})
+			.fail(function() {
+				contenidos.text('Error! La página solicitada no se ha podido encontrar.');
+				estados.text(estados.html() + 'Error\n');
+			})
+			.always(function() {
+				estados.text(estados.html() + 'Finished\n');
+			});
+	});
+	
+	limpiar.on('click', function() {
+		contenidos.text('');
+		estados.text('');
+	});
+
+});
